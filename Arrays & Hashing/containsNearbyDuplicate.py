1897daf8fdaaa1fa18443028a832480518dc83c4
Bekbol Shiribaiev <bekbolnyc@gmail.com>
2025-05-20 11:06:57 -0400

    update

---
+++ Arrays & Hashing/containsNearbyDuplicate.py
@@ -0,0 +1,18 @@
+"""
+link to the problem:
+https://leetcode.com/problems/contains-duplicate-ii/description/
+
+date: May 20, 2025
+"""
+
+def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        temp = {}
+
+        for i in range(len(nums)):
+            if nums[i] in temp:
+                if abs(temp[nums[i]] - i) <= k:
+                    return True
+                temp[nums[i]] = i    
+            else:        
+                temp[nums[i]] = i
+        return False
