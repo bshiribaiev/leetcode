4efb00a90d5748e2fed04302a2c77721d767475c
Bekbol Shiribaiev <bekbolnyc@gmail.com>
2025-05-25 20:06:47 -0400

    maxProduct

---
+++ Arrays & Hashing/maximumProduct.py
@@ -0,0 +1,18 @@
+"""
+link to the problem:
+https://leetcode.com/problems/maximum-product-of-three-numbers/description/
+
+date: May 25, 2025
+"""
+
+from typing import List
+
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        product1 = nums[-1] * nums[-2] * nums[-3]
+        product2 = nums[0] * nums[1] * nums[-1]
+        if product1 > product2:
+            return product1
+        else:
+            return product2
---
+++ Arrays & Hashing/productExceptSelf.py
@@ -0,0 +1,8 @@
+"""
+link to the problem:
+https://leetcode.com/problems/product-of-array-except-self/description/
+
+date: May 25, 2025
+"""
+
+from typing import List
