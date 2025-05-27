8f344da863578ae77e4e527138974bd8b4a3dc23
Bekbol Shiribaiev <bekbolnyc@gmail.com>
2025-05-27 16:58:53 -0400

    solve array exceptself

--- Arrays & Hashing/productExceptSelf.py
+++ Arrays & Hashing/productExceptSelf.py
@@ -2,7 +2,28 @@
 link to the problem:
 https://leetcode.com/problems/product-of-array-except-self/description/
 
-date: May 25, 2025
+date: May 27, 2025
 """
 
 from typing import List
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [1] * n
+
+        prefix = 1
+
+        for i in range(n):
+            res[i] *= prefix
+            prefix *= nums[i]
+        
+        suffix = 1
+        for i in range(n-1, -1, -1):
+            res[i] *= suffix
+            suffix *= nums[i]
+
+        
+        return res
+
+
